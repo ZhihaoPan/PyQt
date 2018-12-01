8527f441b2d548a10c881045fa012b7a2046ce60
pzh <511984189@qq.com>
2018-12-01 17:57:46 +0800

    some modify
    Signed-off-by: pzh <511984189@qq.com>

--- pyQt/mainframepkg/mainWindow.py
+++ pyQt/mainframepkg/mainWindow.py
@@ -25,6 +25,7 @@ class windowMainProc(QMainWindow,Ui_MainWindow):
         self.setupUi(self)
         self.lineEdit.setText(fileName)
         self.lineEdit_2.setText(fileNum)
+
         #设置第一个Timer用于界面显示后的监控模型显示,网络情况每10s一次检测，其他信息每秒检测一次
         self.timer1=QTimer()
         #初始化监控的线程
@@ -33,16 +34,44 @@ class windowMainProc(QMainWindow,Ui_MainWindow):
         self.timer1.start()
         self.timer1.timeout.connect(self.showCurrentTime)
 
+        self.timer2=QTimer()
+        self.timer2.start(5000)
+        self.timer2.timeout.connect(self.sendMsg)
+
+        self.dicContent = {}#todo 将处理文件的信息都按字典形式存入到dicContent中用于后续的发送信息
+
     def showCurrentTime(self):
         currentTime = time.asctime(time.localtime(time.time()))
         self.lineEdit_11.setText(currentTime)
         self.work4monitor.start()
 
     def showMonitor(self,monitorMsg):
+        """
+        显示监控信息
+        :param monitorMsg:
+        :return:
+        """
         self.lineEdit_6.setText(monitorMsg["cpu"])
         self.textEdit.setText(monitorMsg["gpu"])
         self.textEdit_3.setText(monitorMsg["mem"])
         self.textEdit_2.setText((monitorMsg["tem"]))
+
+    def sendtempMsg(self):
+        """
+        发送每5s的信息
+        :return:
+        """
+        self.work4zmq = WorkThread4Zmq(self.dicContent)
+        self.work4zmq.trigger(self.showMsg)
+
+    def showMsg(self,retMsg):
+        """
+        本地发送数据给平台后在此写入日志同时显示在界面上的框内
+        :param retMsg:
+        :return:
+        """
+        pass
+
 class WorkThread4Monitor(QThread):
     trigger = pyqtSignal(dict)
     def __init__(self):
@@ -55,6 +84,14 @@ class WorkThread4Monitor(QThread):
         tem=getTemstate()
         dic = dict(zip(["cpu", "gpu", "mem", "tem"], [cpu, gpu, mem, tem]))
         self.trigger.emit(dic)
+
+class WorkThread4Zmq(QThread):
+    trigger = pyqtSignal(dict)
+
+    def __init__(self,dicContent):
+        pass
+    def run(self):
+        pass
 
 
 if __name__=="__main__":
